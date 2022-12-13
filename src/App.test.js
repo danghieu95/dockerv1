22adaff1fe9861d4350e7eaf8220b97805df2f96
danghieu95 <hieu.dv219128@sis.hust.edu.vn>
2022-12-13 21:15:26 +0700

    4234

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/project1/i);
+  const linkElement = screen.getByText(/cntt/i);
   expect(linkElement).toBeInTheDocument();
 });
