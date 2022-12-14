795116abfaf273646f44069dc18681cd33e52041
danghieu95 <hieu.dv219128@sis.hust.edu.vn>
2022-12-14 18:51:42 +0700

    123

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           CNTT K66 VB2: PROJECT1 !
         </p>
-        <a className="App-link" href="https://www.youtube.com/watch?v=7fQ-mksbty8">Youtube</a>
+        <a className="App-link" href="https://www.youtube.com/watch?v=HF6UZ9YsNI0">Youtube</a>
       </header>
     </div>
   );
