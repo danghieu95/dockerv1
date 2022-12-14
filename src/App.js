6f4773b67f9397e061251803ea23fd278144fc1f
danghieu95 <hieu.dv219128@sis.hust.edu.vn>
2022-12-14 15:26:02 +0700

    4234

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CNTT K66 VB2: PROJECT 1!
+          CNTT K66 VB2: HELLOWORLD!
         </p>
       </header>
     </div>
