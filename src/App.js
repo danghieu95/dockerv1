5f9a410a71181d29e131099e34a031096b976bf0
danghieu95 <hieu.dv219128@sis.hust.edu.vn>
2022-12-14 17:18:33 +0700

    TEST

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CNTT K66 VB2: HELLOWORLD!
+          CNTT K66 VB2: PROJECT1!
         </p>
         <a className="App-link" href="https://www.youtube.com/watch?v=w-sQRS-Lc9k">Youtube - music</a>
       </header>
